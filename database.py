80f230f38b6758e8a152dc6ff2a809dd39ad9a32
Nischal Jain <89198650+Nis-chal-Jain@users.noreply.github.com>
2025-08-17 09:11:37 +0000

    state codes

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
-DB_URL = "mysql+mysqlconnector://root:Vinayak%401844@localhost:3306/statathon"
+# MySQL connection configuration
+DB_URL = "mysql+mysqlconnector://root:root@localhost:3306/microdata"
 
 engine = create_engine(DB_URL)
 LocalSession = sessionmaker(bind=engine)
 Base = declarative_base()   
 
-Tablename = "output"
+TABLE_NAME = "microdata_op"
+STATE_TABLE_NAME = "codes"
