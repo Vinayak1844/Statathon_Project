c9fba062b919fa488c14a7e55f02a70c3c5d90d1
vinayak1844 <vishwakarmavinayak67@gmail.com>
2025-08-17 15:18:39 +0530

    db changes

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # MySQL connection configuration
-DB_URL = "mysql+mysqlconnector://root:root@localhost:3306/microdata"
+DB_URL = "mysql+mysqlconnector://root:Vinayak%401844@localhost:3306/statathon"
 
 engine = create_engine(DB_URL)
 LocalSession = sessionmaker(bind=engine)
